3e769ddbe55fa989c522721746c69aedabc9a018
JaimeMrF <jaime.ja203@gmail.com>
2026-02-19 11:02:05 -0500

    Rutas y Controlador basico

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// HOME
+
+Route::get('/', HomeController::class);
+
+// PRODUCT 
+
+Route::get('/product', [ProductController::class, "index"]);
+
+Route::get('/product', [ProductController::class, "create"]);
+
+Route::get('/product/{producto}', [ProductController::class, "show"]);
+
+
